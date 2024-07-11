0ec2a75d2469d26cc7cb089f7dd2eb787d35292c
Sanders Tshinyama <114026040+Sanders493@users.noreply.github.com>
2024-07-11 22:29:11 +0000

    U6S1P3-P4 solved

--- unit6Session1.py
+++ unit6Session1.py
@@ -19,6 +19,67 @@ def count_element(head: Node, val: object) -> int:
   return count
 
 
-node1 = Node(3, Node(1, Node(2, Node(1))))
-print(count_element(node1, 1))
+# node1 = Node(3, Node(1, Node(2, Node(1))))
+# print(count_element(node1, 1))
 # 3 -> 1 -> 2 -> 1
+
+# Problem 3: Remove Tail
+
+
+# Helper function to print the linked list
+def print_list(node):
+  current = node
+  while current:
+    print(current.value, end=" -> " if current.next else "")
+    current = current.next
+  print()
+
+
+# I have a bug!
+def remove_tail(head):
+  if head is None:  # If the list is empty, return None
+    return None
+  if head.next is None:  # If there's only one node, removing it leaves the list empty
+    return None
+
+# Start from the head and find the second-to-last node
+  current = head
+  while current.next.next:
+    current = current.next
+
+  current.next = None  # Remove the last node by setting second-to-last node to None
+  return head
+
+
+# node1 = Node(1, Node(2, Node(3, Node(4))))
+# print_list(node1)
+# remove_tail(node1)
+# print_list(node1)
+
+# Problem 4: Find the middle
+
+
+def get_ll_length(head: Node | None) -> int:
+  length = 0
+  current: Node | None = head
+  while current:
+    length += 1
+    current = current.next
+  return length
+
+
+def find_middle_element(head) -> object:
+  length: int = get_ll_length(head)
+
+  fast = head
+  slow = head
+
+  while fast.next and fast.next.next :
+    slow = slow.next
+    fast = fast.next.next
+
+  return slow.value if length % 2 != 0 else slow.next.value
+
+
+# node1 = Node(1, Node(2, Node(3)))
+# print(find_middle_element(node1))
