807cad08d3d555bdd81d7982de13ef3d7002ad08
Sanders Tshinyama <114026040+Sanders493@users.noreply.github.com>
2024-07-15 23:51:18 +0000

    U6S2P2 solved

--- unit6Session2.py
+++ unit6Session2.py
@@ -9,7 +9,7 @@ class Node:
 
 
 def is_circular(head):
-  if not head.next:
+  if not head:
     return False
 
   current = head.next
@@ -20,14 +20,45 @@ def is_circular(head):
     current = current.next
   return False
 
-node3 = Node(3)
-node2 = Node(2, node3)
-node1 = Node(1, node2)
-node3.next = node1
 
+# node3 = Node(3)
+# node2 = Node(2, node3)
+# node1 = Node(1, node2)
+# node3.next = node1
 # print(is_circular(node1))
 
 # Problem 2: Find Last Node in a Linked List Cycle
 
-# def find_last_node_in_cycle(head):
-  
+
+def find_last_node_in_cycle(head) -> Node | None:
+  if not head:
+    return None
+  slow = head
+  fast = head
+
+  while fast.next and fast.next.next:
+    fast = fast.next.next
+    slow = slow.next
+    if slow == fast:
+      break
+  else: 
+    return None
+
+  slow = head
+  while slow != fast:
+      slow = slow.next
+      fast = fast.next
+
+
+  last_node = slow
+  while last_node.next != slow:
+      last_node = last_node.next
+
+  return last_node.value
+
+# node4 = Node(4)
+# node3 = Node(3, node4)
+# node2 = Node(2, node3)
+# node1 = Node(1, node2)
+# node4.next = node2
+# print(find_last_node_in_cycle(node1))
