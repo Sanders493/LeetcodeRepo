98b5dfa542abee2d605da037cc08cc7ad1edad50
Sanders Tshinyama <114026040+Sanders493@users.noreply.github.com>
2024-07-18 17:53:52 +0000

    U7S1P5 solved

--- unit7Session1.py
+++ unit7Session1.py
@@ -19,31 +19,62 @@ def repeat_hello_iterative(n):
 
 # Problem 2: Factorial Cases
 
+
 def factorial(n: int) -> int:
   if n == 1 or n == 0:
     return 1
-  
+
   return n * factorial(n - 1)
+
 
 # print(factorial(5))
 
 # Problem 3: Recursive Sum
+
 
 def sum_list(lst: list[int]) -> int:
   if not lst:
     return 0
   return lst[0] + sum_list(lst[1:])
 
+
 # print(sum_list([1,2,3,4,5]))
 
 # Problem 4: Recursive Power of 2
 
+
 def is_power_of_two(n: int | float) -> bool:
-  if n == 2 or n == 1:
+  if n == 1:
     return True
-  if n % 2 != 0:
+  if n % 2 != 0 or n < 1:
     return False
 
   return is_power_of_two(n / 2)
 
+
 # print(is_power_of_two(1024))
+
+# Problem 5: Binary Search I
+
+def binary_search(lst, target) -> int:
+  left = 0
+  right = len(lst) - 1
+  
+  while left <= right:
+    mid = (left + right) // 2
+    if target == lst[mid]:
+      return mid
+    elif target < lst[mid]:
+      right = mid - 1
+    else:
+      left = mid + 1
+
+  return -1
+
+print(binary_search([1,2,3,4,6], 5))
+
+# Problem 6: Backwards Binary Search
+
+
+  
+
