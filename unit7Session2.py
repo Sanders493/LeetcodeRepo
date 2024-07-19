c36668448072e2a3c9d997070e5d0173b89346f4
Sanders Tshinyama <114026040+Sanders493@users.noreply.github.com>
2024-07-19 23:01:40 +0000

    U7S2P3 solved

--- unit7Session2.py
+++ unit7Session2.py
@@ -38,7 +38,7 @@ def count_ones(lst: list[int]) -> int:
 
   return count
 
-print(count_ones([0,0,0,0,1,1,1]))
+# print(count_ones([0,0,0,0,1,1,1]))
 '''
 Happy Case:
 count_ones([0,0,0,0,1,1,1]) => 3
@@ -50,4 +50,43 @@ count_ones([0,0,2,5,13,16,21]) => 0
 count_ones([1,1,1,1]) => 4
 '''
 
-# 
+# Problem 3: Binary Search IV
+def binary_search_helper(nums, target, left, right):
+  if left > right:
+    return -1
+  mid = (left + right) // 2
+  if target == nums[mid]:
+    return mid
+  elif target < nums[mid]:
+    return binary_search_helper(nums, target, left, mid - 1)
+  else:
+    return binary_search_helper(nums, target, mid + 1, right)
+
+def binary_search(nums, target):
+  if not nums:
+    return -1
+  left, right = 0, len(nums) - 1
+  return binary_search_helper(nums, target, left, right)
+
+print(binary_search([1,1,1,3,4], 1))
+
+'''
+Happy Case:
+binary_search([1,2,3,4], 2) => 1
+
+Edge Case:
+binary_search([1,2,3,4], 5) => -1
+binary_search([], 3) => -1
+binary_search([1,1,1,3,4], 1) => 2
+
+Plans:
+-Recalls the function with slicings of the list
+-Make an helper method that also takes in the left and right parameters
+
+Detailed Plan:
+- check if the list is not empty
+-in the driver function initialize the left and right parameters with 0, and len(lst) - 1
+-pass them to the helper method with the list and the target 
+- the main base case would be if the left and right pointers haven't met yet
+- if they haven't i would recall the function with left or right updated 
+'''
