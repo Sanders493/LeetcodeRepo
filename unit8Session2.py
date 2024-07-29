0a353e4b0d5f547d9fb1c9a8027843a0564f5abe
Sanders Tshinyama <114026040+Sanders493@users.noreply.github.com>
2024-07-29 23:04:28 +0000

    U8S1P4 solved

--- unit8Session2.py
+++ unit8Session2.py
@@ -1,4 +1,5 @@
 class TreeNode():
+
    def __init__(self, value, left=None, right=None):
       self.val = value
       self.left = left
@@ -14,6 +15,7 @@ def is_even(root):
    else:
       return True
 
+
 # nod1 = TreeNode(10)
 # nod1.right = TreeNode(8)
 # nod1.left = TreeNode(2)
@@ -23,49 +25,56 @@ def is_even(root):
 
 # print(is_even(nod1))
 
+
 # Problem 2.1: Binary Tree Height
 def height(root: TreeNode | None) -> int:
    if not root:
       return 0
    print(root.val)
    return max(height(root.left), height(root.right)) + 1
-   
+
+
 # print(height(nod1))
+
 
 # Problem 2.2: Binary Tree Max
 def tree_max(root: TreeNode | None) -> TreeNode | None:
    if not root:
       return None
    elements: list[TreeNode] = []
+
    def helper(node: TreeNode | None, lst: list[TreeNode]):
       if node:
          helper(node.left, lst)
          lst.append(node)
          helper(node.right, lst)
-         
+
    helper(root, elements)
-   
+
    max_val: float = float("-inf")
    max_node: TreeNode | None = None
-   
+
    for node in elements:
       if node.val > max_val:
          max_val = node.val
          max_node = node
-         
+
    return max_node
 
+
 # print(tree_max(nod1).val)
-      
+
 
 class TreeNode():
-  def __init__(self, key, value, left=None, right=None):
+
+   def __init__(self, key, value, left=None, right=None):
       self.key = key
       self.val = value
       self.left = left
       self.right = right
 
-# Problem 3: BST Insert 
+
+# Problem 3: BST Insert
 def insert(root: TreeNode | None, key: int, value: object):
    if not root:
       return None
@@ -86,35 +95,63 @@ def insert(root: TreeNode | None, key: int, value: object):
          current = current.right
 
    return root
-   
-node1 = TreeNode(10, "Neiji")
-node1.left = TreeNode(8, "Sasuke")
-node1.right = TreeNode(15, "Itachi")
-node1.left.left = TreeNode(1, "Kakashi")
-node1.left.right = TreeNode(6, "Ino")
+
+
+# node1 = TreeNode(10, "Neiji")
+# node1.left = TreeNode(8, "Sasuke")
+# node1.right = TreeNode(15, "Itachi")
+# node1.left.left = TreeNode(1, "Kakashi")
+# node1.left.right = TreeNode(6, "Ino")
 
 # insert(node1, 0, "Naruto")
 # print(node1.left.left.left.val)
 
+
 # Problem 4: BST Remove I
+def find_smallest_child(root: TreeNode) -> TreeNode:
+   min_node: TreeNode = TreeNode(float("inf"), "temp")
+
+   def helper(node: TreeNode | None) -> None:
+      nonlocal min_node
+      if node:
+         if node.key < min_node.key:
+            min_node = node
+         helper(node.right)
+         helper(node.left)
+
+   helper(root)
+
+   return min_node
+
+
 def remove_bst(root: TreeNode | None, key: int) -> TreeNode | None:
    if not root:
       return None
 
-   current = root
-   while current:
-      if current.key == key:
-         pass
-      elif current.key > key:
-         current = current.
-   # Locate the node to be removed
-   # If the node is a leaf node:
-      # Remove the node by redirecting the appropriate child reference of its parent to None
-   # If the node has one parent:
-      # Replace the node with its child, updating its parent's nodes child reference appropriately
-   # If the node has two children:
-      # Find the node's inorder successor (smallest node in right subtree)
-      # Swap the value of the node and its inorder successor
-      # Recursively remove the successor (which now has the current node's value)
-   # Return the root of the updated tree
-   
+   if root.key > key:
+      root.left = remove_bst(root.left, key)
+   elif root.key < key:
+      root.right = remove_bst(root.right, key)
+   else:
+      if not root.left:
+         return root.right
+      if not root.right:
+         return root.left
+
+      temp_node: TreeNode = find_smallest_child(root.right)
+      root.key = temp_node.key
+      root.val = temp_node.val
+      root.right = remove_bst(root.right, temp_node.key)
+      
+   return root
+
+
+node1 = TreeNode(10, "Neiji")
+node1.left = TreeNode(8, "Sasuke")
+node1.right = TreeNode(15, "Itachi")
+node1.left.left = TreeNode(1, "Kakashi")
+node1.left.right = TreeNode(9, "Ino")
+node1.right.left = TreeNode(13, "Madara")
+node1.right.right = TreeNode(17, "Shoji")
+remove_bst(node1, 10)
+print(node1.val)
